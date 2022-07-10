d9c987d9aad87275e40cf3cf9aca3a43052e0e6c
hsiang <50895141+hsiangchengfun@users.noreply.github.com>
2022-07-10 16:21:20 +0800

    Time: 52 ms (21.66%), Space: 31 MB (87.58%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -4,10 +4,8 @@ public:
         int tt=0;
         for(int i=0;i<nums.size();i++)tt+=nums[i];
         int now=0;
-        // cout<<tt<<endl;
         for(int i=0;i<nums.size();i++){
             
-            // cout<<"now "<<now<<" nums "<<nums[i]<<endl;
             if(now*2 == tt-nums[i]){
                 return i;
             }
