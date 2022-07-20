9fd4ca4ff24832a8d184631f3fbdd9c6c26dc232
hsiang <50895141+hsiangchengfun@users.noreply.github.com>
2022-07-20 13:56:15 +0800

    Time: 8 ms (13.85%), Space: 6.5 MB (28.52%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -6,14 +6,14 @@ public:
             if(t.length()< 1 )return false;
 
             auto itr = t.find(s[0]);
-            cout<<itr<<endl;
+            
             if(itr == std::string::npos)return false;
             else{
                 s.erase(0,1);
                 t.erase(0,itr+1); 
                 
             }
-            cout<<s<<endl<<t<<endl;
+            
             
         }
         return true;
