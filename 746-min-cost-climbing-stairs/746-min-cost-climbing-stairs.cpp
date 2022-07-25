1d421ddd359304f849f3dd817730c98b58eeeaa5
hsiang <50895141+hsiangchengfun@users.noreply.github.com>
2022-07-25 14:59:47 +0800

    Time: 17 ms (17.61%), Space: 13.7 MB (77.69%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,10 +4,8 @@ public:
         int i=2;
         for(i=2;i<cost.size();i++){
             cost[i] += min( cost[i-1],cost[i-2] ); 
-            // for(int j=0;j<cost.size();j++)cout<<cost[j]<<" ";
-            // cout<<endl;
+         
         }
-        // i++;
         int ans;
         ans = min(cost[i-1],cost[i-2]);
         
