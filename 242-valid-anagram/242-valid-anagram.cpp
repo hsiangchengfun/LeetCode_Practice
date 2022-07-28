f63ca4f33703bb96811b8cf21a2a3c96e3015eec
hsiang <50895141+hsiangchengfun@users.noreply.github.com>
2022-07-29 00:59:40 +0800

    Time: 25 ms (16.79%), Space: 7.5 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,24 +3,27 @@ public:
     bool isAnagram(string s, string t) {
         vector<char> v1(s.length()),v2(t.length());
         
-        for(int i=0;i<s.length();i++){
-            v1[i] = s[i];
-        }
-        for(int i=0;i<t.length();i++){
-            v2[i] = t[i];
-        }
+//         for(int i=0;i<s.length();i++){
+//             v1[i] = s[i];
+//         }
+//         for(int i=0;i<t.length();i++){
+//             v2[i] = t[i];
+//         }
         
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
-        string a ,b;
-        for(int i=0;i<v1.size();i++){
-            a+=v1[i];
-        }
-        for(int i=0;i<v2.size();i++){
-            b+=v2[i];
-        }
+//         sort(v1.begin(),v1.end());
+//         sort(v2.begin(),v2.end());
+//         string a ,b;
+//         for(int i=0;i<v1.size();i++){
+//             a+=v1[i];
+//         }
+//         for(int i=0;i<v2.size();i++){
+//             b+=v2[i];
+//         }
         
-        if(a==b)return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        if(s==t)return true;
         else return false;
         
         
