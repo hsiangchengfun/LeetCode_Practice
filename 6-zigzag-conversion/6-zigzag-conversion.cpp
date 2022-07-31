2a743b5514f14dacdd606f7a43d009a9d0b4fcfa
hsiang <50895141+hsiangchengfun@users.noreply.github.com>
2022-07-31 21:53:02 +0800

    Time: 16 ms (59.94%), Space: 10.8 MB (39.96%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<string> vec(numRows);
+        int i=0;
+        while(i<s.length()){
+            for(int j=0;j<numRows && i<s.length();j++,i++) vec[j]+=s[i];
+            for(int j=numRows-2;j > 0 && i<s.length();j--,i++) vec[j]+=s[i];
+            
+            
+        }
+        string ans;
+        for(i = 0;i<numRows;i++){
+           ans+=vec[i];
+        }
+        return ans;
+    }
+};
