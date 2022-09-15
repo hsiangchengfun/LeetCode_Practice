b3cb5a110c8d6eb773e665f435c21888ca72f13c
hsiang <50895141+hsiangchengfun@users.noreply.github.com>
2022-09-15 11:02:49 +0800

    Time: 23 ms (74.59%), Space: 16.6 MB (80.48%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -5,19 +5,20 @@ public:
         int dest=0,start=0,step=0,nextdest=0;
         int n = nums.size();
         while(dest < n-1 ){
-            step++;
             nextdest = dest + 1;
             
             for(int i=start;i<=dest;i++){
                 
                 nextdest = max( nextdest , nums[i]+i );
-                if( nextdest >= n-1 )return step;
+                if( nextdest >= n-1 )return step + 1;
                 
             }
             
             
             start = dest + 1;
             dest = nextdest;
+            step++;
+
         }
         return step;
         
